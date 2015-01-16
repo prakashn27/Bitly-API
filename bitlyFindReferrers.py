ba04cd658f0e1a1d1f2e65fc121d4a49ae7dafbd
Prakash Natarajan <nprakashece@gmail.com>
2015-01-16 08:57:09 -0500

    pep-8 style update

--- bitlyFindReferrers.py
+++ bitlyFindReferrers.py
@@ -13,7 +13,7 @@ import json
 	
 def FindReferrers(bitlyURL)
 	query_params = {'access_token': 'API_KEY',
-                'link': bitlyURL}
+                	'link': bitlyURL}
 
 	endpoint = 'https://api-ssl.bitly.com/v3/link/referrers'
 	response = requests.get(endpoint, params=query_params, verify=False)
