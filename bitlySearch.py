78c201f79a1d7da82d9b3285fb2c3a917f93c2aa
Prakash Natarajan <nprakashece@gmail.com>
2015-01-16 08:57:52 -0500

    Update bitlySearch.py

--- bitlySearch.py
+++ bitlySearch.py
@@ -13,8 +13,8 @@ import json
 
 def Search(keyword)
 	query_params = {'access_token': 'API_KEY',
-        'query': keyword,
-        'limit': 1}
+        		'query': keyword,
+        		'limit': 1}
 
 	endpoint = 'https://api-ssl.bitly.com/v3/search'
 	response = requests.get(endpoint, params=query_params, verify=False)
